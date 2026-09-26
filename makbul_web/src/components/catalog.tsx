2e4ff8dbd61333b195d8b8a6a2de2bda7e265a06
Yunus Emre <yunusmertofficial@gmail.com>
2026-09-26 13:31:09 +0300

    Sultan cilekli duble lokum eklendi

--- makbul_web/src/components/catalog.tsx
+++ makbul_web/src/components/catalog.tsx
@@ -25,6 +25,7 @@ const NEW_PRODUCT_IDS = new Set([
   "223", "224", "225", "226", "227", "228", "229", "230",
   "231", "232", "233", "234",
   "235",
+  "236",
 ]);
 
 function isNewProduct(product: Product) {
